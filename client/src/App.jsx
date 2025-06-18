bccfd7242dc325647294bdd17504d293b6fceaf2
kc9600 <48585506+kc9600@users.noreply.github.com>
2025-06-18 23:38:13 +0530

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+export default App;
